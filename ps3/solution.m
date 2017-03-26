147ae65f3aa4de330ff505890c167a25a5872963
Yinghua Zhu <zhuyinghua1203@hotmail.com>
2017-03-25 22:44:30 -0700

    updated question ps3-1b

--- ps3/solution.m
+++ ps3/solution.m
@@ -1,4 +1,4 @@
-% Solution 1a
+%% Solution 1a
 clear, clc;
 
 pts3d_norm = dlmread('../../input/ps3/pts3d-norm.txt');
@@ -21,8 +21,8 @@ end
 [~,~,V] = svd(A' * A);
 
 M = V(:, end);
-disp('M is');
 M = reshape(M, 4, 3)';
+disp('M is');
 disp(M);    % scaled equivalent to the matrix in ps3
 
 last_pt = M * [pts3d_norm(end, :), 1]';
@@ -30,8 +30,71 @@ last_pt = last_pt/last_pt(3);   % go to inhomogeneous
 disp(['Projected point: ', num2str(last_pt(1:2)')]);
 % disp(['Ground truth: ', num2str(pts2d_norm_pic(end, :))]);
 disp(['Residual: ', num2str(norm(last_pt(1:2)' - pts2d_norm_pic(end, :)))]);
-%%
-% 1c
+
+%% Solution 1b
+clear;
+
+pts3d = dlmread('../../input/ps3/pts3d.txt');
+pts2d_pic_b = dlmread('../../input/ps3/pts2d-pic_b.txt');
+
+all_norms = [];
+num_rep = 10;
+min_norm = realmax;
+
+for k = [8, 12, 16]
+    mean_norms = zeros(num_rep, 1);
+    
+    for n_rep = 1 : num_rep
+        randnums = randperm(20);
+        A = zeros(k*2, 12);
+
+        % use k points to construct the matrix
+        for i = 1 : k
+            j = randnums(i);
+            A((i-1)*2+1, 1:3) = pts3d(j, :);
+            A((i-1)*2+1, 4) = 1;
+            A((i-1)*2+1, 9:11) = -pts2d_pic_b(j, 1) * pts3d(j, :);
+            A((i-1)*2+1, 12) = -pts2d_pic_b(j, 1);
+
+            A(i*2, 5:7) = pts3d(j, :);
+            A(i*2, 8) = 1;
+            A(i*2, 9:11) = -pts2d_pic_b(j, 2) * pts3d(j, :);
+            A(i*2, 12) = -pts2d_pic_b(j, 2);
+        end
+
+        [~,~,V] = svd(A' * A);
+
+        M = V(:, end);
+        M = reshape(M, 4, 3)';
+
+        % 4 unused points
+        last_pts = M * [pts3d(end-3 : end, :), ones(4, 1)]';
+        last_pts = last_pts./repmat(last_pts(3, :), 3, 1);   % go to inhomogeneous
+
+        % calculate residuals
+        norms = zeros(1, 4);
+        for i = 1 : 4
+            norms(i) = norm(last_pts(1:2, i) - pts2d_pic_b(20-(4-i), :)');
+        end
+        mean_norm = mean(norms);
+
+        mean_norms(n_rep) = mean_norm;
+        
+        % record the best M
+        if mean_norm < min_norm
+            min_norm = mean_norm;
+            best_M = M;
+        end
+    end
+    all_norms = cat(2, all_norms, mean_norms);
+end
+
+disp('Average residual:');
+disp(all_norms);
+disp('Best M:');
+disp(best_M);
+
+%% Solution 1c
 Q = m(:, 1:3);
 C = -inv(Q)*m(:, 4);
 
